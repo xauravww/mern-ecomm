6d98b7f1a46fb62349a7a8aae8a18fc357fcb90e
xauravww <sauravmaheshwari8@gmail.com>
2024-04-23 18:31:05 +0530

    Fiexd: origin frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 app.use(cors({
     credentials:true,
-    origin: ['http://localhost:5500','http://127.0.0.1:5500','http://localhost:3000','https://mern-ecomm-backend-gi43.onrender.com']
+    origin: ['http://localhost:5500','http://127.0.0.1:5500','http://localhost:3000','https://mern-ecomm-gco0.onrender.com']
     }));
 app.use(express.json())
 app.use(cookieParser())
